122dbdbb2befe2520ee0977fbada72575e6c1b9e
Andreas Wenzel <awenzel5@uoregon.edu>
2023-08-25 22:14:54 -0700

    separate install and scan

--- secret-setup/scan.sh
+++ secret-setup/scan.sh
@@ -1,33 +1,16 @@
 #!/usr/bin/env sh
-
-echo "PWD $(pwd)"
-
 cd $GITHUB_WORKSPACE
 cd ..
 
-echo "PWD $(pwd)"
-echo "ls $(ls -al)"
-
 echo "Cloning git-secrets code from github"
+echo "PWD $(pwd)"
 git clone https://github.com/awslabs/git-secrets.git
-
-# echo "Installing git-secrets"
-# cd git-secrets && make install && 
 
 cd $GITHUB_WORKSPACE
 echo "PWD $(pwd)"
-echo "ls $(ls -al)"
+
+echo "installing git secrets into repo"
+../git-secrets/git-secrets --install
 
 echo "Adding pattern to catch"
 ../git-secrets/git-secrets --add-provider -- cat ./secret-setup/secret-regex
-
-echo "installing into repo"
-../git-secrets/git-secrets --install
-
-../git-secrets/git-secrets --scan
-# printenv
-
-# for commit in $(git rev-list ${{ github.base_ref }}..${{ github.head_ref }}); do
-#     git checkout $commit
-#     echo "Running git-secrets"
-# done
